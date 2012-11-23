0ea91be87d616f572be8bbc26e56f2e9c330062c
SlaterTest <Ska@1Pax.com>
2012-11-23 15:12:31 +0100

    erster Test

---
+++ EFMVC.Web.Core/Models/Role.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EFMVC.Web.Core.Models
+{
+    public class Roles
+    {
+        public const string Admin = "Admin";
+        public const string Manager = "Manager";
+        public const string User = "User";  
+    }
+    public enum UserRoles
+    {
+        Admin = 1,
+        Manager = 2,
+        User = 3       
+    }
+}
